6dd73c7c37088db02e909f9b35b1d4323cd9290a
unknown <asyl06kair@gmail.com>
2022-05-24 19:07:34 +0600

    feat: homePage and routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "normalize.css";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyles } from "./global-styles";
+import { app } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <FirebaseContext.provider value={{ app }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.provider>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,22 @@
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { seedDatabase } from "../seed";
+
+
+// we need config here
+
+const config = {
+  apiKey: "AIzaSyBP-aS2rxsH2BG4T5Bbt_OGWuCpDDR0rmw",
+  authDomain: "netflix2-799bf.firebaseapp.com",
+  projectId: "netflix2-799bf",
+  storageBucket: "netflix2-799bf.appspot.com",
+  messagingSenderId: "193541802613",
+  appId: "1:193541802613:web:318d609362291d4f3f463c",
+};
+
+const app = firebase.initializeApp(config);
+// seedDatabase(app)
+
+export { app };
