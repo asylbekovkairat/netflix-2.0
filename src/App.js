c1dbe36d1d30f0290e6b2b157c3929173a2cc4e3
unknown <asyl06kair@gmail.com>
2022-06-24 15:21:04 +0600

    bugfix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home, Signin, Signup, Browse } from "./pages";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 import { useAuthListener } from "./hooks";
 
-export default function App() {
+export default function app() {
   const {user} = useAuthListener();
   
   return (
